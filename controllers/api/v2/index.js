cde50d2ff92e7700eec1946b01daa33a411725d1
Niles Bingham <nbing19@gmail.com>
2022-06-27 21:41:48 -0700

    fixes #5, added redis caching to get routes

--- controllers/api/v2/index.js
+++ controllers/api/v2/index.js
@@ -2,9 +2,21 @@ const router = require("express").Router();
 const productRoutes = require("./products");
 const reviewRoutes = require("./reviews");
 const categoryRoutes = require("./categories");
+const { client } = require("../../../config/redis");
 
-router.use((req, res, next) => {
-  if (req.method === "GET") res.set("Cache-Control", "private, max-age=300");
+router.use(async (req, res, next) => {
+  // await client.connect();
+
+  if (req.method === "GET") {
+    res.set("Cache-Control", "private, max-age=300");
+
+    const cache = await client.get(req.originalUrl);
+
+    if (cache) {
+      res.status(200).json(JSON.parse(cache));
+      return;
+    }
+  }
   next();
 });
 
